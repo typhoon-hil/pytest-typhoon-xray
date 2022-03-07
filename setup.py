b8f761170b8bb07280f6ffe4d770e40a88bb8ab2
Branko Milosavljevic <mbranko@uns.ac.rs>
2022-03-07 19:06:18 +0100

    Bump version: 0.4.3 → 0.4.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-typhoon-xray',
-    version='0.4.3',
+    version='0.4.4',
     author='Branko Milosavljevic',
     author_email='branko@typhoon-hil.com',
     maintainer='Branko Milosavljevic',
